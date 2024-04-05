f9ce9aa32abacd5d170455817095f5ccd5b2230c
Abdelrhman Mohsen <abdulrahman_20230317@fci.helwan.edu.eg>
2024-04-05 05:47:38 +0200

    Faster navigation through landing Images

--- main.js
+++ main.js
@@ -59,10 +59,20 @@ document.onclick = (e) => {
 };
 /*=========================================*/
 // landing page images bullets
+const imageUrls = [];
 let landig_imgs = [];
 for (let i = 0; i < 6; ++i){
+    imageUrls[i] = `Images/homeImgs/Landing-Page-${i + 1}.jpg`
     landig_imgs[i] = document.getElementById(`land-${i + 1}`);
 }
+// add images to the page on load
+window.onload = () => {
+    imageUrls.forEach(url =>{
+        const imgElement = document.createElement('img');
+        imgElement.style.display = 'none';
+        imgElement.src = url;
+    });
+};
 let last_bullet = 0;
 for (let i = 0; i < 6; ++i){
     landig_imgs[i].onclick = () => {
