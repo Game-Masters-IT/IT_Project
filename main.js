e6e19f097561ad603af967d479512f9315c01d4a
Abdelrhman Mohsen <abdulrahman_20230317@fci.helwan.edu.eg>
2024-04-20 14:24:16 +0200

    Cart V2.0 [Linking to Game Product Page]

--- main.js
+++ main.js
@@ -58,3 +58,7 @@ document.onclick = (e) => {
     }
 };
 /*=========================================*/
+let cartIconHeader = document.querySelector('.fa-bag-shopping');
+cartIconHeader.classList.remove('fa-bag-shopping');
+cartIconHeader.classList.add('fa-cart-shopping');
+document.getElementById('cart-counter').innerHTML = Object.keys(JSON.parse(localStorage.getItem('cart-games'))).length;
